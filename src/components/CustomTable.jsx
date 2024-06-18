1f794e18ca432d347110e166e9b8fcb1b292dd4b
lakshman <laxmankumar929@gmail.com>
2024-06-18 07:52:44 +0530

    fixed user info issues

--- src/components/CustomTable.jsx
+++ src/components/CustomTable.jsx
@@ -7,6 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel'
 import GlobalStyles from '../theme/GlobalStyles';
 import {useEffect} from 'react'
 import '../styles/table.css';
