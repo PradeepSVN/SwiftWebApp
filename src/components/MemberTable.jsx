bb0085c927aa29089b12b1615c6b57922f115303
M v v satyanarayana <venkatasatyanarayana.m@inteqsolutions.com>
2024-07-02 09:12:17 +0530

    userlist search aliginment fixed

--- src/components/MemberTable.jsx
+++ src/components/MemberTable.jsx
@@ -123,7 +123,7 @@ export default function StickyHeadTable({tableData,handleMemberInfo,handlePagina
       {/* <TableContainer sx={{ maxHeight: 740 }}> */}
       <TableContainer >
       <Table stickyHeader aria-label="sticky table" className='customTable'
-        style={{width:'99%', marginRight:'5px', justifyContent:'center',alignContent:'center',alignItems:'center'}}>
+        style={{width:'100%', justifyContent:'center',alignContent:'center',alignItems:'center'}}>
           {/* <TableHead>
             <TableRow className='table-header'>
               {columns.map((column) => (
